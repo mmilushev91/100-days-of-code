3ded6c2c1cf256a66fb63fb56841b02790cb25d8
mmilushev91 <milushev_1990@abv.bg>
2025-08-29 15:07:31 +0300

    Create data_types.py

---
+++ Day 2 - Data types, Numbers, Operations, Type conversition, f-strings/Lecture/data_types.py
@@ -0,0 +1,27 @@
+#Python Primitive Data Types
+
+#1. Strings
+
+"String" #indexes: 0 1 2 3 4 5
+
+#Subscripting
+print("String"[0]) # S
+
+#challenge: print the last character of "Hello" String
+print("Hello"[4]) # o
+#negative indexes start counting from backwards
+print("Hello"[-1]) # o
+#everything inside double or single quotes is considered as String, even numbers
+print("123" + "345") # 123345. The process of joining strings is called concatenation
+
+#2. Integers = int = whole numbers
+print(123 + 345) # 468
+
+#3. Large Numbers - _ can be used between thousands for better visiability
+print(123_456_789)
+
+#4 Floats
+print(3.1456) # 3.1456
+
+#5 Booleans - value can either true or false
+print(True, False) # True False
