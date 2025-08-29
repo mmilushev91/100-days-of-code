b2c8ecf7df03f74b00df82db367cefde4daa1a1e
mmilushev91 <milushev_1990@abv.bg>
2025-08-29 21:49:35 +0300

    Update tip_calculator.py

--- Day 2 - Data types, Numbers, Operations, Type conversition, f-strings/Project/tip_calculator.py
+++ Day 2 - Data types, Numbers, Operations, Type conversition, f-strings/Project/tip_calculator.py
@@ -2,7 +2,7 @@ print("Welcome to the tip calculator!")
  
 bill = int(input("What was the total bill? $"))
 tip_percent = int(input("How much tip would you like to give? 10, 12, or 15? "))
-people_count = int(input("How many people to split the bill?"))
+people_count = int(input("How many people to split the bill? "))
 
 tip_amount = tip_percent / 100 * bill
 bill_plus_tip = bill + tip_amount
