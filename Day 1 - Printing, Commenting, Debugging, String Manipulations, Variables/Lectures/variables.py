21dfc4b58a183e85cdcc1e7017ca43a2632f2e87
mmilushev91 <milushev_1990@abv.bg>
2025-08-29 12:53:10 +0300

    Create variables.py

---
+++ Day 1 - Printing, Commenting, Debugging, String Manipulations, Variables/Lectures/variables.py
@@ -0,0 +1,22 @@
+#Variables - containers, that allows us to lable a piece of data so we can use the 
+#name to refference to the stored data.
+
+#we can use the input function to take user input, which we can store in a variable
+name = input("What is your name: ")
+#we can then use the stored data
+print(name);
+
+#we can also hardcore values in variables
+name2 = "Marian"
+print(name2)
+#we can also change the value of the variable later
+name2 = "Angela"
+print(name2)
+
+#challenge print the length of an inputed string
+print(len(input("Enter a string: ")))
+#challenge do above challenge with variables
+username = input("What is your name: ")
+length = len(username)
+
+print(length)
