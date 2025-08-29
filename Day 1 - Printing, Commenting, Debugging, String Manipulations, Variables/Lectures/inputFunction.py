272cf797eeb1075c0978c443e9362e47a9d9ac9e
mmilushev91 <milushev_1990@abv.bg>
2025-08-29 12:01:53 +0300

    Create inputFunction.py

---
+++ Day 1 - Printing, Commenting, Debugging, String Manipulations, Variables/Lectures/inputFunction.py
@@ -0,0 +1,10 @@
+#Python input function
+#Lecture
+print("Hello " + input("What is your name: "))
+
+#Challenge: add ! at the of the printed line
+print("Hello " + input("What is your name: ") + "!")
+#Install and use thonny application. It shows the steps of program one after the other
+
+# # - symbol is used for commenting. Comments are skipped by the computer and are used
+# to tell the developers what some line or block of code is used or leave notes
