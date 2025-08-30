44d44393e0f5475462847424dffcab94bbaed3e1
mmilushev91 <milushev_1990@abv.bg>
2025-08-30 17:31:24 +0300

    Update nested_control_flows.py

--- Day 3 - Conditional statements, Logical operators, Code blocks and Scope/Lectures/nested_control_flows.py
+++ Day 3 - Conditional statements, Logical operators, Code blocks and Scope/Lectures/nested_control_flows.py
@@ -1,8 +1,6 @@
-#Nested conditionals and elif
-
+#Nested conditionals and elif. Multiple if-s
 print("Welcome to the rollercoaster")
-height = int(input("What is your height? "))
-
+height = int(input("What is your height in cm? "))
 if height >= 120:
     print("You can get on the rollercoaster!")
     
@@ -11,11 +9,19 @@ if height >= 120:
     
     if age <= 12:
         price = 5
+        print("Child ticket is $5.")
     elif age <= 18:
         price = 7
+        print("Youth ticket is $7")
     else:
         price = 12 
+        print("Adult ticket is $12")
+    
+    want_photo = input("Do you want a photo (yes/no)? ")
+    
+    if want_photo == "yes":
+        price += 3
         
-    print(f"Ticket price is: ${price}")
+    print(f"Final price is: ${price}")
 else:
     print("You have to grow up to use the rollercoaster!")
