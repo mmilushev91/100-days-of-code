f770aadf5ab783abde6cdfe865c53db626fe9191
mmilushev91 <milushev_1990@abv.bg>
2025-09-01 13:40:14 +0300

    Update get_flag.py

--- Day 6 - Functions, scope, while loops/Exercises/get_flag.py
+++ Day 6 - Functions, scope, while loops/Exercises/get_flag.py
@@ -2,19 +2,16 @@ def turn_right():
     for _ in range(3):
         turn_left()
 def step_over():
+    move()
     turn_left()
     move()
     turn_right()
     move()
     turn_right()
     move()
+    turn_left()
     
-steps = 5
-move()
+steps = 6
 
 for _ in range(steps):
     step_over()
-    turn_left()
-    move()
-
-step_over()
