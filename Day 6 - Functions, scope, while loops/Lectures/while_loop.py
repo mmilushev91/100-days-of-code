f9a8424163b845373ebc135601c455ea0e53adf1
mmilushev91 <milushev_1990@abv.bg>
2025-09-02 09:46:46 +0300

    Create while_loop.py

---
+++ Day 6 - Functions, scope, while loops/Lectures/while_loop.py
@@ -0,0 +1,16 @@
+def turn_right():
+    for _ in range(3):
+        turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    jump()
