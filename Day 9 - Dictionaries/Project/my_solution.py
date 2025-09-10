dbf5f267705aa97bfeb2d9a4a7f1a3acc2e8e59d
mmilushev91 <milushev_1990@abv.bg>
2025-09-10 10:55:47 +0300

    Create my_solution.py

---
+++ Day 9 - Dictionaries/Project/my_solution.py
@@ -0,0 +1,50 @@
+def validate_amount():
+    amount = input("What's bid amount: $")
+
+    while not amount.isnumeric():
+        print("Please enter a positive number!")
+        amount = input("What's bid amount: $")
+
+    return float(amount)
+
+def validate_answer():
+    answer = input("Is there other bidder. Type 'yes' or 'no': ").lower()
+    
+    while answer != "yes" and answer != "no":
+        answer = input("Is there other bidder? Type 'yes' or 'no': ").lower()
+    
+    return answer
+
+def find_highest_bidder(data):
+    max_bid = 0
+    winner_name = ""
+    
+    for item in data:
+        
+        if data[item] > max_bid:
+            max_bid = data[item]
+            winner_name = item
+    
+    return [winner_name, max_bid]
+    
+#replace with logo
+print("Welcome to the Secret Auction House")
+
+auction_house = {}
+auction_continues = True
+
+while auction_continues:
+    
+    name = input("What's bidder name: ")
+    amount = validate_amount()
+    auction_house[name] = amount
+    
+    new_bidder = validate_answer()
+    
+    if new_bidder == "no":
+        auction_continues = False
+
+winner, bid = find_highest_bidder(auction_house)
+
+print(f"The winner is {winner} with ${bid:.2f}!")
+
