a5e4098d412eaf149c746ab3a44b19c6691d4ba6
mmilushev91 <milushev_1990@abv.bg>
2025-09-18 11:28:15 +0300

    Update my_solution.py

--- Day 15- Coffee Machine Project /my_solution.py
+++ Day 15- Coffee Machine Project /my_solution.py
@@ -30,9 +30,9 @@ def validate_coin_amount(coin_name):
 def calculate_paid():
     print("Please insert coins: ")
     paid = validate_coin_amount("quaters") * 0.25
-    dimes += validate_coin_amount("dimes") * 0.10
-    nickles = validate_coin_amount("nickles") * 0.05
-    pennies = validate_coin_amount("pennies") * 0.01
+    paid += validate_coin_amount("dimes") * 0.10
+    paid += validate_coin_amount("nickles") * 0.05
+    paid += validate_coin_amount("pennies") * 0.01
 
     return paid
     
@@ -40,6 +40,30 @@ def deduct_resources(drink, resources):
     for ingredient in drink["ingredients"]:
         resource = drink["ingredients"][ingredient]
         resources[ingredient] -= resource
+
+def is_order_completed(drink, resources, money, act):
+    
+    if are_resources_enough(drink, resources):
+        paid = calculate_paid()       
+        cost = drink["cost"]
+                
+        if paid < cost:
+            print("Sorry that's not enough money. Money refunded.")
+            return False
+                    
+        deduct_resources(drink, resources)
+        change = 0
+                    
+        if paid > cost:
+            change = paid - cost
+            print(f"Here is ${change:.2f} dollars in change.")
+            
+        money["value"] += cost
+        print(f"Here is your {act}. Enjoy!")
+        return True
+    
+    return False
+        
 
 def coffee_machine():
     machine_resources = {
@@ -58,32 +82,14 @@ def coffee_machine():
         action = input("What would you like? (espresso/latte/cappuccino): ").lower()
         
         if action == "off":
-            machine_on = False
-                    
+            machine_on = False       
+            
         elif action == "report":
-            print_report(resources=machine_resources, money=machine_money)
-    
+            print_report(resources=machine_resources, money=machine_money) 
+            
         elif action in ["espresso", "latte", "cappuccino"]:
             choicen_drink = MENU[action]
-           
-            if are_resources_enough(drink=choicen_drink, resources=machine_resources):
-                money_paid = calculate_paid()       
-                cost = choicen_drink["cost"]
-                
-                if money_paid < cost:
-                    print("Sorry that's not enough money. Money refunded.")
-                    continue
-                
-                deduct_resources(drink=choicen_drink, resources=machine_resources)
-                change = 0
-                
-                if money_paid > cost:
-                    change = paid - cost
-                    print(f"Here is ${change:.2f} dollars in change.")
-                
-                machine_money["value"] += cost
-                print(f"Here is your {action}. Enjoy!")
-         
+            is_order_completed(drink=choicen_drink, resources=machine_resources, money=machine_money, act=action)
         else:
             print("Invalid action!")
 
