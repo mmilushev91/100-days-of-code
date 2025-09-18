7fbba0c5b1efcf6c801054cd980563a1ee89e546
mmilushev91 <milushev_1990@abv.bg>
2025-09-18 15:05:45 +0300

    Update lecture.py

--- Day 16 - /lecture.py
+++ Day 16 - /lecture.py
@@ -1,32 +1,27 @@
-"""The procedural approach in programming is a paradigm that structures code into a series of
-step-by-step instructions or procedures. It emphasizes a top-down design, where tasks are broken into smaller, 
-reusable functions or routines, making it easier to manage and understand. This approach is foundational for
+from turtle import Turtle, Screen
 
-Object-Oriented Programming (OOP) is a programming paradigm that organizes software design around objects,
-which are instances of classes. These objects encapsulate data (attributes) and behavior (methods),
-enabling modular, reusable, and maintainable code.
-
-Key Concepts of OOP
-
-Classes and Objects: A class is a blueprint for creating objects, defining their attributes and methods.
-For example, a Car class might define properties like color and speed and methods like drive() or brake(). 
-An object is an instance of a class. For example, myCar = Car() creates an object myCar with specific attributes
-and behaviors.
-
-Encapsulation: Encapsulation involves bundling data and methods within an object and restricting direct access 
-to some components. This ensures data hiding and protects the integrity of the object.
-
-Inheritance: Inheritance allows a class (child) to derive properties and methods from another class (parent). 
-For instance, a SportsCar class can inherit from a Car class and add unique features like turboBoost().
-
-Polymorphism: Polymorphism enables objects to take on multiple forms. For example, a draw() method
-might behave differently for Circle and Rectangle objects, even though both inherit from a common Shape class.
-
-Abstraction: Abstraction hides complex implementation details and exposes only essential features. 
-For example, a Car class might provide a start() method without revealing the internal workings of the engine.
-"""
-
-from turtle import Turtle 
-
+#creating turtle object
 timmy = Turtle()
 print(timmy)
+#change shape of turtle
+timmy.shape("turtle")
+#change color of turtle
+timmy.color("coral")
+#move turtle forward with distance
+timmy.forward(100)
+#creating screen object
+my_screen = Screen()
+#returns the height of the screen
+print(my_screen.canvheight)
+#exit the screen when clicked
+my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+#creating table object
+table = PrettyTable()
+#adding columns to the table
+table.add_column(fieldname="Pokemon Name", column=["Pikachu", "Squirtle", "Charmander"])
+table.add_column(fieldname="Type", column=["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
