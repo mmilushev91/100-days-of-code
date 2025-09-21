b71d81d796ecd7abc23ed9f6a7f4f2987ae46c15
mmilushev91 <milushev_1990@abv.bg>
2025-09-21 16:53:14 +0300

    Create random_walk.py

---
+++ Day 17 - Turtle Graphical Interface/random_walk.py
@@ -0,0 +1,28 @@
+from turtle import Turtle, Screen
+from random import choice
+
+colors = [
+    "red", "green", "blue", "yellow", "orange", "purple", "pink",
+    "black", "white", "brown", "gray", "cyan", "magenta", "gold",
+    "silver", "violet", "indigo", "lime", "turquoise", "navy",
+    "maroon", "beige", "tan", "chocolate", "coral", "salmon"
+]
+
+headings = [0, 90, 180, 270]
+
+step = Turtle()
+step_length = 50
+walk_length = 100
+
+step.pensize(5)
+#make repetitive
+for _ in range(walk_length):
+    # set random color
+    step.color(choice(colors))
+    # set random direction
+    step.setheading(choice(headings))
+    # move
+    step.forward(step_length)
+
+screen = Screen()
+screen.exitonclick()
