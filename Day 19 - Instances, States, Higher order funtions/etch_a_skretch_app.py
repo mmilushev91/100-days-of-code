5c0d749ccad6ca95467200c9932e91adf5219481
mmilushev91 <milushev_1990@abv.bg>
2025-09-24 11:59:18 +0300

    Create etch_a_skretch_app.py

---
+++ Day 19 - Instances, States, Higher order funtions/etch_a_skretch_app.py
@@ -0,0 +1,30 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+  tim.forward(10)
+  
+def move_backwards():
+  tim.backward(10)
+
+def turn_right():
+  tim.right(10)
+  
+def turn_left():
+  tim.left(10)
+
+def clear_drawing():
+  tim.reset()
+
+
+#adding event listener to screen object to listen for events
+screen.listen()
+#higher order function is a function that takes another function as an input
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear_drawing)
+screen.exitonclick()
