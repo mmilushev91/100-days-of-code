7ec45f2df3ba39611f11d02a7223a5c7d7d6497e
mmilushev91 <milushev_1990@abv.bg>
2025-09-29 11:59:37 +0300

    Update main.py

--- Day 21 - Inharitance - Snake game part 2/main.py
+++ Day 21 - Inharitance - Snake game part 2/main.py
@@ -1,6 +1,5 @@
 from time import sleep
 from turtle import Screen
-
 from scoreboard import Scoreboard
 from snake import Snake
 from food import Food
@@ -24,19 +23,30 @@ screen.onkey(fun=snake.right, key="d")
 game_on = True
 
 while game_on:
-    screen.update()
-    sleep(0.1)
+  screen.update()
+  sleep(0.1)
 
-    snake.move()
+  snake.move()
 
-    #Detect collusion with food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        scoreboard.update_score()
-        snake.ate = True
-        snake.eat()
-    else:
-        snake.ate = False
+  #Detect collusion with food
+  if snake.head.distance(food) < 15:
+    food.refresh()
+    scoreboard.update_score()
+    snake.ate = True
+    snake.eat()
+  else:
+      snake.ate = False
 
+  #Detect collusion with wall
+  if snake.head.xcor() >= 290 or snake.head.xcor() <= -290 or \
+    snake.head.ycor() >= 290 or snake.head.ycor() <= -290:
+    scoreboard.game_over()
+    game_on = False
+
+  #Detect collusion with tail
+
+  if snake.tail_collusion():
+    scoreboard.game_over()
+    game_on = False
 
 screen.exitonclick()
