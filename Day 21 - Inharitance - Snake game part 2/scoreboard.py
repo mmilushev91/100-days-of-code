a38a5d176e6a268ad8a146fa9118a01805c9122b
mmilushev91 <milushev_1990@abv.bg>
2025-09-29 11:57:57 +0300

    Update scoreboard.py

--- Day 21 - Inharitance - Snake game part 2/scoreboard.py
+++ Day 21 - Inharitance - Snake game part 2/scoreboard.py
@@ -1,5 +1,8 @@
 from turtle import Turtle
 
+ALIGN = "center"
+FONT = ('Arial', 16, 'normal')
+
 class Scoreboard(Turtle):
 
     def __init__(self):
@@ -12,9 +15,13 @@ class Scoreboard(Turtle):
         self.display_score()
 
     def display_score(self):
-        self.write(arg=f"Score: {self.score}", align="center", font=('Arial', 16, 'normal'))
+        self.write(arg=f"Score: {self.score}", align=ALIGN, font=FONT)
 
     def update_score(self):
         self.score += 1
         self.clear()
         self.display_score()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(arg="GAME OVER", align=ALIGN, font=FONT)
