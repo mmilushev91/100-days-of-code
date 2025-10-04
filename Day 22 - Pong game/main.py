1059320fa322d0436597946219185a091ad92962
mmilushev91 <milushev_1990@abv.bg>
2025-10-04 19:15:57 +0300

    Update main.py

--- Day 22 - Pong game/main.py
+++ Day 22 - Pong game/main.py
@@ -7,24 +7,24 @@ from time import sleep
 PLAYER_1_START_X = -350
 PLAYER_2_START_X = 350
 
-#Create screen
+# Create screen
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong Game")
 screen.tracer(0)
 
-#Create paddles
+# Create paddles
 player_1 = Paddle(start_x=PLAYER_1_START_X)
 player_2 = Paddle(start_x=PLAYER_2_START_X)
 
-#Create ball
+# Create ball
 ball = Ball()
 
-#Create scoreboard
+# Create scoreboard
 scoreboard = Scoreboard()
 
-#Move paddles
+# Move paddles
 screen.listen()
 screen.onkey(fun=player_1.up, key="w")
 screen.onkey(fun=player_1.down, key="s")
@@ -32,26 +32,33 @@ screen.onkey(fun=player_2.up, key="Up")
 screen.onkey(fun=player_2.down, key="Down")
 
 game_on = True
+sleep_time = 0.1
 
 while game_on:
-  
-  sleep(0.1)
-  screen.update()
-  #Move the ball
-  ball.move()
 
-  #Detect colusion with wall and bounce
-  if ball.ycor() > 280 or ball.ycor() < -280:
-    ball.bounce_y()
+    sleep(sleep_time)
+    screen.update()
+    # Move the ball
+    ball.move()
 
-  #Detect colusion with paddle
-  if ball.distance(player_1) <= 50 and ball.xcor() > 340 or \
-    ball.distance(player_2) <= 50 and ball.xcor() < -340:
-    ball.bounce_x()
+    # Detect colusion with wall and bounce
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect colusion with paddle
+    if ball.distance(player_1) <= 50 and ball.xcor() < -320 or \
+            ball.distance(player_2) <= 50 and ball.xcor() > 320:
+        ball.bounce_x()
+        sleep_time -= 0.01
+
+    # Detect when paddle misses
+    if ball.xcor() > 380:
+        scoreboard.increase_player1_score()
+        ball.reset()
+        sleep_time = 0.1
+    elif ball.xcor() < -350:
+        scoreboard.increase_player2_score()
+        ball.reset()
+        sleep_time = 0.1
 
 screen.exitonclick()
-
-
-#Detect colusion with paddle
-#Detect when paddle misses
-#Keep score
