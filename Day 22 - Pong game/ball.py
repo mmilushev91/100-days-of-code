9aa0c5e090f6e07dfdaa1671482c86b2d6a23e8b
mmilushev91 <milushev_1990@abv.bg>
2025-10-04 19:16:44 +0300

    Update ball.py

--- Day 22 - Pong game/ball.py
+++ Day 22 - Pong game/ball.py
@@ -4,28 +4,29 @@ UP = 90
 DOWN = 180
 
 class Ball(Turtle):
-  def __init__(self):
-    super().__init__()
-    self.create()
-    
-  def create(self):
-    self.penup()
-    self.color("white")
-    self.shape("circle")
-    self.x_move = 10
-    self.y_move = 10
+    def __init__(self):
+        super().__init__()
+        self.x_move = 10
+        self.y_move = 10
+        self.create()
 
-  def bounce_x(self):
-    self.x_move *= -1
+    def create(self):
+        self.penup()
+        self.color("white")
+        self.shape("circle")
 
-  def bounce_y(self):
-    self.y_move *= -1
+    def bounce_x(self):
+        self.x_move *= -1
 
-  def move(self):
-    new_x = self.xcor() + self.x_move
-    new_y = self.ycor() + self.y_move
+    def bounce_y(self):
+        self.y_move *= -1
 
-    self.goto(x=new_x, y=new_y)
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
 
+        self.goto(x=new_x, y=new_y)
 
-    
+    def reset(self):
+        self.home()
+        self.bounce_x()
