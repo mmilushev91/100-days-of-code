350cc6775b50c24b49e324299ee99c7ea602e5be
mmilushev91 <milushev_1990@abv.bg>
2025-10-07 17:19:41 +0300

    Create scoreboard.py

---
+++ Day 24 - Working with files and files paths/adding highscore to snake game/scoreboard.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.highscore = self.get_highscore()
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.highscore}", align=ALIGNMENT, font=FONT)
+
+    def save_highscore(self):
+        with open(file="data.txt", mode="w") as file:
+            file.write(str(self.score))
+
+    def get_highscore(self):
+        with open(file="data.txt", mode="r") as file:
+            highscore = file.read()
+
+        return int(highscore)
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            self.save_highscore()
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
